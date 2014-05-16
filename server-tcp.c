770b2c2ff27521d8c13e883595b3176eace0418d
Shane Israel <shane@wolfssl.com>
2014-05-16 10:24:20 -0600

    Made the server more simplified and added better comments

--- server-tcp.c
+++ server-tcp.c
@@ -20,84 +20,88 @@
  * =============================================================================
  *
  * This is a super basic example of what a TCP Server might look like that is
- * not actively using any form of security.
+ * not actively using any form of security. 
  */
+
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE = 256
+#define DEFAULT_PORT = 11111
 
 int main(int argc, char *argv[])
 {
-    int                 sockfd, newsockfd, port, clilen;
-    char                buffer[256];
-    struct sockaddr_in  serv_addr, cli_addr;
-    int                 n;
+    /* Identify and access the sockets */
+    int socketd, connd;
 
-    /* Initialize the socket */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    
-    /* If the socket fails, throw an error and kill the server
-     * with exit status 1 */
-    if(sockfd < 0)
+    /* Server and Client socket address structures */
+    struct sockaddr_in server_addr, client_addr;
+
+    /* Buffer to store data recieved from the client */
+    char buff[MAX_SIZE];
+
+    /* Creates a socket that uses an internet IP address */
+    /* Sets the type to be Stream based (TCP)            */
+    /* 0 means choose the default protocol.              */
+    socketd = socket(AF_INET, SOCK_STREAM, 0);
+
+    /* If the socket descriptor (socketd) is a positive
+       value then we know it is valid                    */
+    if(socketd < 0)
     {
-        perror("ERROR opening the socket");
+        printf("ERROR: failed to create the socket\n");
+        /* Kill the server with exit status 1 */
         exit(1);
     }
 
-    /* Setup the socket */
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    port                            = 11111;     /* set the default port */
-    serv_addr.sin_family            = AF_INET;
-    serv_addr.sin_addr.serv_addr    = INADDR_ANY;
-    serv_addr.sin_port              = htons(port);
+    /* We now need to initialize the server address struct to zero */
+    bzero((char *)&server_addr, sizeof(server_addr));
 
-    if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-    {
-        perror("ERROR on binding");
-        exit(1);
-    }
-
-    /* Start listening for clients, the server will enter sleep mode and
-     * wait for an incoming connection from the client */
-    listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
-
-    /* Accept the connection from the client */
-    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-
-    /* Just as above, if the accept connection fails, we throw an error and
-     * kill the server with exit status 1 */
-    if(newsockfd < 0)
-    {
-        perror("ERROR on accept");
-        exit(1);
-    }
+    /* Fill the server's address family */
+    server_addr.sin_family              = AF_INET;
+    server_addr.sin_addr.s_addr         = INADDR_ANY;
+    /* Set the default port */
+    server_addr.sin_port                = DEFAULT_PORT;
     
-    /* If the connection does not fail and is established then we can now
-     * start communicating with the connected client */
-    bzero(buffer, 256);
-    
-    /* read the data sent from the client */
-    n = read(newsockfd, buffer, 255);
+    /* Now we need to attch the server socket to our port. Doing this enforces
+       that it does not select a port randomly but instead uses our specified
+       port. If we return a value less than 0, then we know it's invalid */
+    if(bind(socketd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+        printf("ERROR: failed to bind\n");
 
+    /* Now we will have the server start listening, it will halt at this point
+       until it recieves a connection from a client. We will also allow it to 
+       hold 5 client connection requests as pending while the server is
+       processing another request */
+    listen(socketd, 5);
+    printf("Waitinf for a connection...\n");
+    int size = sizeof(client_addr);
     
-    if(n < 0)
-    {
-        perror("ERROR reading from the socket");
-        exit(1);
-    }
+    /* The server will wait at this point until a client tries to establish a
+       connection. Once a connection is established it returns a new
+       'connected socket descriptor' different from the one created earlier */
+    conn_desc = accept(socketd, (struct sockaddr *)&client_addr, &size);
+    if(conn_desc == -1)
+        printf("Failed to accept the connection\n");
+    else
+        printf("Connected Successfully!");
 
-    /* print the data from the client to the console */
-    printf("Client: %s\n", buffer);
+    /* This new descriptor can now be read from or written to just like a 
+       normal file descriptor */
+    if(read(conn_desc, buff, sizeof(buff)-1) > 0)
+        printf("Data Recieved: %s", buff);
+    else
+        printf("Failed to recieve data\n");
 
-    /* send a response back to the client */
-    n = write(newsockfd, "TCP Server checking in!");
-    if(n < 0)
-    {
-        perror("ERROR writing to socket");
-        exit(1);
-    }
-    
+    /* At this point the program is finished and you should now close all 
+       open sockets that we used. */
+    close(conn_desc);
+    close(socketd);
     return 0;
+
 }
