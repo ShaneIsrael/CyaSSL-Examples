2530362ecf1f19bfaea4b0e6e0b894c03d2aea94
Shane Israel <shane@wolfssl.com>
2014-05-20 13:10:49 -0600

    moved main method to the bottom of the file

--- secured/server-tcp.c
+++ secured/server-tcp.c
@@ -48,74 +48,12 @@ struct sockaddr_in server_addr, client_addr;
 /* Create a ctx pointer for our ssl */
 CYASSL_CTX* ctx;
 
-int main(int argc, char *argv[])
-{
-    /* initialize CyaSSL */
-    CyaSSL_Init();
-
-    /* create and initialize CYASSL_CTX structure */
-    if((ctx = CyaSSL_CTX_new(CyaTLSv1_2_server_method())) == NULL)
-    {
-        fprintf(stderr, "CyaSSL_CTX_new error.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    /* Load server certificate into CYASSL_CTX */
-    if(CyaSSL_CTX_use_certificate_file(ctx, "certs/server-cert.pem", SSL_FILETYPE_PEM) != SSL_SUCCESS)
-    {
-        fprintf(stderr, "Error loading certs/server-cert.pem, please check the file.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    /* Load server key into CYASSL_CTX */
-    if(CyaSSL_CTX_use_PrivateKey_file(ctx, "certs/server-key.pem", SSL_FILETYPE_PEM) != SSL_SUCCESS)
-    {
-        fprintf(stderr, "Error loading certs/server-key.pem, please check the file.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    /* 
-     * Creates a socket that uses an internet IP address,
-     * Sets the type to be Stream based (TCP),
-     * 0 means choose the default protocol.
-     */
-    socketd = socket(AF_INET, SOCK_STREAM, 0);
-
-    /* If positive value, the socket is valid */
-    if(socketd < 0)
-    {
-        printf("ERROR: failed to create the socket\n");
-        exit(1);        /* Kill the server with exit status 1 */        
-    }
-
-    /* Initialize the server address struct to zero */
-    bzero((char *)&server_addr, sizeof(server_addr)); 
-
-    /* Fill the server's address family */
-    server_addr.sin_family          = AF_INET;
-    server_addr.sin_addr.s_addr     = INADDR_ANY;
-    server_addr.sin_port            = htons(DEFAULT_PORT);
-
-    /* Attach the server socket to our port */
-    if(bind(socketd, (struct sockaddr *)&server_addr, sizeof(server_addr))
-        < 0)
-    {
-        printf("ERROR: failed to bind\n");
-        exit(1);
-    }
-
-    /* Accept client connections and read from them */
-    AcceptAndRead();
-
-    CyaSSL_CTX_free(ctx);   /* Free CYASSL_CTX */
-    CyaSSL_Cleanup();       /* Free CyaSSL */
-    exit(EXIT_SUCCESS);
-}
 void AcceptAndRead()
 {
     int exit_status = 0; /* 0 = false, 1 = true */
 
-    /* Continuously accept connects while not currently in an active connection or told to quit */
+    /* Continuously accept connects while not currently in an active connection
+       or told to quit */
     while (exit_status == 0)
     {
         /* listen for a new connection, allow 5 pending connections */
@@ -178,5 +116,72 @@ void AcceptAndRead()
         CyaSSL_free(ssl);           /* Free the CYASSL object */
         close(connd);               /* close the connected socket */
     }
+}
 
+int main(int argc, char *argv[])
+{
+    /* initialize CyaSSL */
+    CyaSSL_Init();
+
+    /* create and initialize CYASSL_CTX structure */
+    if((ctx = CyaSSL_CTX_new(CyaTLSv1_2_server_method())) == NULL)
+    {
+        fprintf(stderr, "CyaSSL_CTX_new error.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Load server certificate into CYASSL_CTX */
+    if(CyaSSL_CTX_use_certificate_file(ctx, "certs/server-cert.pem", 
+                SSL_FILETYPE_PEM) != SSL_SUCCESS)
+    {
+        fprintf(stderr, "Error loading certs/server-cert.pem, please check the",
+               " file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Load server key into CYASSL_CTX */
+    if(CyaSSL_CTX_use_PrivateKey_file(ctx, "certs/server-key.pem", 
+                SSL_FILETYPE_PEM) != SSL_SUCCESS)
+    {
+        fprintf(stderr, "Error loading certs/server-key.pem, please check the ",
+                "file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* 
+     * Creates a socket that uses an internet IP address,
+     * Sets the type to be Stream based (TCP),
+     * 0 means choose the default protocol.
+     */
+    socketd = socket(AF_INET, SOCK_STREAM, 0);
+
+    /* If positive value, the socket is valid */
+    if(socketd < 0)
+    {
+        printf("ERROR: failed to create the socket\n");
+        exit(1);        /* Kill the server with exit status 1 */        
+    }
+
+    /* Initialize the server address struct to zero */
+    bzero((char *)&server_addr, sizeof(server_addr)); 
+
+    /* Fill the server's address family */
+    server_addr.sin_family          = AF_INET;
+    server_addr.sin_addr.s_addr     = INADDR_ANY;
+    server_addr.sin_port            = htons(DEFAULT_PORT);
+
+    /* Attach the server socket to our port */
+    if(bind(socketd, (struct sockaddr *)&server_addr, sizeof(server_addr))
+        < 0)
+    {
+        printf("ERROR: failed to bind\n");
+        exit(1);
+    }
+
+    /* Accept client connections and read from them */
+    AcceptAndRead();
+
+    CyaSSL_CTX_free(ctx);   /* Free CYASSL_CTX */
+    CyaSSL_Cleanup();       /* Free CyaSSL */
+    exit(EXIT_SUCCESS);
 }
